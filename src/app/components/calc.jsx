a7104133f7b54f4bc0b53d224bf16918ae04f8b7
Eric Eiselt <ericeiselt@mbp-von-eric.home>
2024-06-19 12:16:05 +0200

    mobileflex

--- src/app/components/calc.jsx
+++ src/app/components/calc.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import SubwayRechner from "@/app/components/subwaycalc";
 import KfcRechner from "@/app/components/kfccalc";
 
@@ -33,8 +33,8 @@ export default function Rechner() {
             </nav>
             <div className="p-4">
 
-                {activeTab === "SubwayRechner" && <SubwayRechner />}
-                {activeTab === "KFC-Rechner" && <KfcRechner />}
+                {activeTab === "SubwayRechner" && <SubwayRechner/>}
+                {activeTab === "KFC-Rechner" && <KfcRechner/>}
             </div>
         </div>
     );
--- src/app/components/subwaycalc.jsx
+++ src/app/components/subwaycalc.jsx
@@ -198,7 +198,7 @@ export default function SubwayRechner() {
                     </select>
                 </label>
             </div>
-<div id="wrapper" className="grid grid-cols-3">
+<div id="wrapper" className="md:grid md:grid-cols-3 flex flex-col">
     <div id="left">
             <div className="mb-8">
                 <label className="block mb-4 font-extrabold text-xl">Gemüse</label>
