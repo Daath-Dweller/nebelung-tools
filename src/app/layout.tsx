b9e2d74d043cce49d651a757d356e229f886613c
Eric Eiselt <ericeiselt@macbook-pro-von-eric.home>
2024-11-07 14:43:03 +0100

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nebelung-Tools",
+  title: "Der Almanach",
   description: "small collection of completely arbitrary tools",
 };
 
