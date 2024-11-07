abded556434db23652739d537f16b2da1367d950
Eric Eiselt <ericeiselt@macbook-pro-von-eric.home>
2024-11-07 14:33:29 +0100

    rename

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -4,7 +4,7 @@ export default function About() {
 
 
     return (
-        <div className="md:p-12 bg-black text-white m-3">
+        <div className="md:p-12 p-4 bg-black text-white m-3">
             <h1 className="text-2xl font-bold mb-2 pl-4 pt-2">Willkommen auf der-Almana.ch. Dies startete als ein Projekt wild zusammengewürfelter Ideen und Dingen, die ich ad hoc nicht in einer guten Version andernorts online fand, aber selbst nutzen wollte. Und das ist es eigentlich auch noch: Eine Spielwiese zum Lernen und Probieren, aber sie gewinnt immer mehr an Struktur.</h1>
 
         </div>
--- src/app/components/lingua.jsx
+++ src/app/components/lingua.jsx
@@ -4,8 +4,8 @@ export default function Lingua() {
 
 
     return (
-        <div className="md:p-12 bg-black text-white m-3">
-            <h1 className="text-2xl font-bold mb-2 pl-4 pt-2">Verbastrum (Verbum für Wort + astrum, „Reich/Umgebung“; eine Art „Wort-Universum“ für Sprach- und Wortwissen)</h1>
+        <div className="md:p-12  bg-black text-white m-3">
+            <h1 className="text-2xl font-bold mb-2 p-4 ">Verbastrum (Verbum für Wort + astrum, „Reich/Umgebung“; eine Art „Wort-Universum“ für Sprach- und Wortwissen)</h1>
 
         </div>
     );
