ce4298175e3cc3b1891ae1c76acb9ff26b4bc01b
Daath-Dweller <eiselt.eric@proton.me>
2024-11-10 15:18:10 +0100

    testpositiv

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -5,7 +5,7 @@ export default function About() {
 
     return (
         <div className="md:p-12 p-4 bg-black text-white m-3">
-            <h1 className="text-2xl font-bold mb-2 pl-4 pt-2">TestWillkommen auf der-Almana.ch. Dies startete als ein Projekt wild zusammengewürfelter Ideen und Dingen, die ich ad hoc nicht in einer guten Version andernorts online fand, aber selbst nutzen wollte. Und das ist es eigentlich auch noch: Eine Spielwiese zum Lernen und Probieren, aber sie gewinnt immer mehr an Struktur.</h1>
+            <h1 className="text-2xl font-bold mb-2 pl-4 pt-2">Willkommen auf der-Almana.ch. Dies startete als ein Projekt wild zusammengewürfelter Ideen und Dingen, die ich ad hoc nicht in einer guten Version andernorts online fand, aber selbst nutzen wollte. Und das ist es eigentlich auch noch: Eine Spielwiese zum Lernen und Probieren, aber sie gewinnt immer mehr an Struktur.</h1>
 
         </div>
     );
