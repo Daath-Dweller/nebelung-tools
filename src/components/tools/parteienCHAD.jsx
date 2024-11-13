14c3d14139b7ae4deb210d621244f46f0dffe9bf
Eric Eiselt <ericeiselt@macbook-pro-von-eric.home>
2024-11-13 15:15:20 +0100

    parteien

--- src/components/tools/parteienCHAD.jsx
+++ src/components/tools/parteienCHAD.jsx
@@ -1,30 +1,51 @@
-// Parteien.jsx
 import React, { useState } from 'react';
 import { countries, regions, parties, seatDistributions } from '@/data/parteiendata.ts';
 
 const Parteien = () => {
     const [selectedCountryId, setSelectedCountryId] = useState(null);
     const [selectedRegionId, setSelectedRegionId] = useState(null);
+    const [selectedGroupAbbr, setSelectedGroupAbbr] = useState(null);
+    const [isInfoVisible, setIsInfoVisible] = useState(false); // Neuer State für Info-Text
 
     const handleCountryChange = (e) => {
         const countryId = parseInt(e.target.value, 10);
         setSelectedCountryId(countryId);
         setSelectedRegionId(null); // Reset region selection
+        setSelectedGroupAbbr(null); // Reset group selection
+        setIsInfoVisible(false); // Reset Info-Text
     };
 
     const handleRegionChange = (e) => {
         const value = e.target.value;
         const regionId = value === '' ? null : parseInt(value, 10);
         setSelectedRegionId(regionId);
+        setSelectedGroupAbbr(null); // Reset group selection
+        setIsInfoVisible(false); // Reset Info-Text
     };
 
-    const filteredRegions = regions.filter((region) => region.countryId === selectedCountryId);
+    const handleGroupButtonClick = (groupAbbr) => {
+        // Toggle group selection
+        setSelectedGroupAbbr((prevGroupAbbr) =>
+            prevGroupAbbr === groupAbbr ? null : groupAbbr
+        );
+        setIsInfoVisible(false); // Reset Info-Text when group changes
+    };
+
+    const handleInfoIconClick = () => {
+        setIsInfoVisible((prev) => !prev);
+    };
+
+    const filteredRegions = regions.filter(
+        (region) => region.countryId === selectedCountryId
+    );
 
     let seatDistributionForDisplay;
 
     if (selectedRegionId === 0) {
-        // "Gesamt" selected
-        const regionsInCountry = regions.filter((region) => region.countryId === selectedCountryId);
+        // "Durchschnitt" selected
+        const regionsInCountry = regions.filter(
+            (region) => region.countryId === selectedCountryId
+        );
         const regionIdsInCountry = regionsInCountry.map((region) => region.id);
 
         // Get all seat distributions for the selected country
@@ -43,7 +64,7 @@ const Parteien = () => {
 
         // Convert to an array for rendering
         seatDistributionForDisplay = Object.keys(seatsPerParty).map((partyId) => ({
-            id: parseInt(partyId, 10), // Using partyId as id
+            id: parseInt(partyId, 10),
             partyId: parseInt(partyId, 10),
             seats: seatsPerParty[partyId],
         }));
@@ -61,6 +82,27 @@ const Parteien = () => {
         seatDistributionForDisplay && seatDistributionForDisplay.length > 0
             ? Math.max(...seatDistributionForDisplay.map((seat) => seat.seats))
             : 0;
+
+    // Calculate the total number of seats
+    const totalSeats = seatDistributionForDisplay.reduce(
+        (sum, seat) => sum + seat.seats,
+        0
+    );
+
+    // Filter parties based on selected group abbreviation
+    const partiesInSelectedGroup = selectedGroupAbbr
+        ? parties.filter(
+            (party) =>
+                party.groupAbbr === selectedGroupAbbr &&
+                party.countryId === selectedCountryId
+        )
+        : [];
+
+    // Extract the group name from the first party
+    const groupName =
+        selectedGroupAbbr && partiesInSelectedGroup.length > 0
+            ? partiesInSelectedGroup[0].group
+            : '';
 
     return (
         <div className="p-12 bg-black text-white m-3">
@@ -89,7 +131,7 @@ const Parteien = () => {
                         className="text-black mt-2 p-1"
                     >
                         <option value="">Wähle Region</option>
-                        <option value="0">Gesamt</option>
+                        <option value="0">Durchschnitt</option>
                         {filteredRegions.map((region) => (
                             <option key={region.id} value={region.id}>
                                 {region.name}
@@ -97,6 +139,22 @@ const Parteien = () => {
                         ))}
                     </select>
                 </>
+            )}
+
+            {/* Sonderfall Appenzell Innerrhoden */}
+            {selectedRegionId === 5 && (
+                <div className="mt-6">
+                    <p>
+                        Appenzell Innerrhoden verfügt über ein einzigartiges politisches System, das
+                        sich von anderen Schweizer Kantonen unterscheidet. Die wichtigsten
+                        kantonalen Entscheidungen, einschließlich Wahlen und Abstimmungen, werden
+                        traditionell an der Landsgemeinde getroffen. Diese jährliche Versammlung
+                        findet unter freiem Himmel statt, wo die Stimmberechtigten durch Handerheben
+                        oder Zuruf über verschiedene Geschäfte entscheiden. Daher gibt es in
+                        Appenzell Innerrhoden keine klassischen kantonalen Wahlergebnisse, wie sie in
+                        anderen Kantonen üblich sind.
+                    </p>
+                </div>
             )}
 
             {/* Display Parties and Seat Distribution */}
@@ -112,6 +170,10 @@ const Parteien = () => {
                         {seatDistributionForDisplay.map((seat) => {
                             const party = parties.find((p) => p.id === seat.partyId);
                             const heightPercent = (seat.seats / maxSeats) * 100;
+                            const percentage =
+                                totalSeats > 0
+                                    ? ((seat.seats / totalSeats) * 100).toFixed(1)
+                                    : 0;
 
                             return (
                                 <div
@@ -121,18 +183,106 @@ const Parteien = () => {
                                         height: `${heightPercent}%`,
                                         backgroundColor: party?.color || '#FFFFFF',
                                     }}
-                                    title={`${party?.name} (${party?.abbreviation}): ${seat.seats} Sitze`}
+                                    title={`${party?.name} (${party?.abbreviation}): ${
+                                        seat.seats
+                                    } Sitze (${percentage}%)`}
                                 >
-                                    {/* Tooltip on hover */}
                                     <div className="absolute bottom-full mb-2 left-0 right-0 hidden group-hover:block">
                                         <div className="bg-gray-800 text-white text-xs rounded py-1 px-2">
-                                            {party?.name} ({party?.abbreviation}): {seat.seats} Sitze
+                                            {party?.name} ({party?.abbreviation}) <br />
+                                            <br /> {seat.seats} Sitze ({percentage}%)
                                         </div>
                                     </div>
                                 </div>
                             );
                         })}
                     </div>
+
+                    {/* Group Buttons */}
+                    <div className="mt-6">
+                        <button
+                            onClick={() => handleGroupButtonClick('TBR')}
+                            className={`px-4 py-2 mr-2 ${
+                                selectedGroupAbbr === 'TBR'
+                                    ? 'bg-blue-500'
+                                    : 'bg-gray-700 hover:bg-gray-600'
+                            }`}
+                        >
+                            TBR
+                        </button>
+                        <button
+                            onClick={() => handleGroupButtonClick('KMUA')}
+                            className={`px-4 py-2 mr-2 ${
+                                selectedGroupAbbr === 'KMUA'
+                                    ? 'bg-blue-500'
+                                    : 'bg-gray-700 hover:bg-gray-600'
+                            }`}
+                        >
+                            KMUA
+                        </button>
+                        <button
+                            onClick={() => handleGroupButtonClick('LGP')}
+                            className={`px-4 py-2 ${
+                                selectedGroupAbbr === 'LGP'
+                                    ? 'bg-blue-500'
+                                    : 'bg-gray-700 hover:bg-gray-600'
+                            }`}
+                        >
+                            LGP
+                        </button>
+                    </div>
+
+                    {/* Display Parties in Selected Group */}
+                    {selectedGroupAbbr && partiesInSelectedGroup.length > 0 && (
+                        <div className="mt-4">
+                            <div className="flex items-center">
+                                <p className="mr-2 font-extrabold">
+                                    {selectedGroupAbbr} ({groupName}):
+                                </p>
+                                { selectedCountryId === 1 && (  <span
+                                    onClick={handleInfoIconClick}
+                                    className="cursor-pointer text-blue-500"
+                                >
+                               {/* Info Icon */}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="M13 16h-1v-4h-1m1-4h.01M12 2a10 10 0 100 20 10 10 0 000-20z"
+                                        />
+                                    </svg>
+                                </span>
+                                )}
+                            </div>
+                            {/* Info Text */}
+                            {isInfoVisible &&  (
+                                <div className="mt-2 text-sm bg-gray-800 p-2 rounded">
+                                    Offizielle Einordnung nach&nbsp;
+                                    <a
+                                        href="https://www.bfs.admin.ch/asset/de/30146543"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="text-blue-400 underline"
+                                    >
+                                       BFS
+                                    </a>
+
+                                </div>
+                            )}
+                            {partiesInSelectedGroup.map((party) => (
+                                <p key={party.id}>
+                                    {party.name} ({party.abbreviation})
+                                </p>
+                            ))}
+                        </div>
+                    )}
                 </div>
             )}
         </div>
--- src/data/parteiendata.ts
+++ src/data/parteiendata.ts
@@ -17,6 +17,8 @@ export interface Party {
     abbreviation: string;
     color: string;
     countryId: number;
+    group: string;
+    groupAbbr: string;
 }
 
 export interface SeatDistribution {
@@ -34,67 +36,155 @@ export const countries: Country[] = [
 
 export const regions: Region[] = [
     // Switzerland Cantons
-    { id: 1, name: 'Zürich', countryId: 1 },
-    { id: 2, name: 'Bern', countryId: 1 },
-    // ... add all 26 cantons
+    { id: 1, name: 'Zürich (ZH)', countryId: 1 },
+    { id: 2, name: 'Zug (ZG)', countryId: 1 },
+    { id: 3, name: 'Aargau (AG)', countryId: 1 },
+    { id: 4, name: 'Appenzell Ausserrhoden (AR)', countryId: 1 },
+    { id: 5, name: 'Appenzell Innerrhoden (AI)', countryId: 1 },
+    { id: 6, name: 'Basel-Landschaft (BL)', countryId: 1 },
+    { id: 7, name: 'Basel-Stadt (BS)', countryId: 1 },
+    { id: 8, name: 'Bern (BE)', countryId: 1 },
+    { id: 9, name: 'Freiburg (FR)', countryId: 1 },
+    { id: 10, name: 'Genf (GE)', countryId: 1 },
+    { id: 11, name: 'Glarus (GL)', countryId: 1 },
+    { id: 12, name: 'Graubünden (GR)', countryId: 1 },
+    { id: 13, name: 'Jura (JU)', countryId: 1 },
+    { id: 14, name: 'Luzern (LU)', countryId: 1 },
+    { id: 15, name: 'Neuenburg (NE)', countryId: 1 },
+    { id: 16, name: 'Nidwalden (NW)', countryId: 1 },
+    { id: 17, name: 'Obwalden (OW)', countryId: 1 },
+    { id: 18, name: 'Schaffhausen (SH)', countryId: 1 },
+    { id: 19, name: 'Schwyz (SZ)', countryId: 1 },
+    { id: 20, name: 'Solothurn (SO)', countryId: 1 },
+    { id: 21, name: 'St. Gallen (SG)', countryId: 1 },
+    { id: 22, name: 'Tessin (TI)', countryId: 1 },
+    { id: 23, name: 'Thurgau (TG)', countryId: 1 },
+    { id: 24, name: 'Uri (UR)', countryId: 1 },
+    { id: 25, name: 'Waadt (VD)', countryId: 1 },
+    { id: 26, name: 'Wallis (VS)', countryId: 1 },
+
 
     // Germany Bundesländer
     { id: 27, name: 'Bayern', countryId: 2 },
     { id: 28, name: 'Berlin', countryId: 2 },
-    { id: 29, name: 'NRW', countryId: 2 },
-    // ... add all 16 Bundesländer
+    { id: 29, name: 'Nordrhein-Westfalen', countryId: 2 },
+    { id: 30, name: 'Baden-Württemberg', countryId: 2 },
+    { id: 31, name: 'Brandenburg', countryId: 2 },
+    { id: 32, name: 'Bremen', countryId: 2 },
+    { id: 33, name: 'Hamburg', countryId: 2 },
+    { id: 34, name: 'Hessen', countryId: 2 },
+    { id: 35, name: 'Mecklenburg-Vorpommern', countryId: 2 },
+    { id: 36, name: 'Niedersachsen', countryId: 2 },
+    { id: 37, name: 'Rheinland-Pfalz', countryId: 2 },
+    { id: 38, name: 'Saarland', countryId: 2 },
+    { id: 39, name: 'Sachsen', countryId: 2 },
+    { id: 40, name: 'Sachsen-Anhalt', countryId: 2 },
+    { id: 41, name: 'Schleswig-Holstein', countryId: 2 },
+    { id: 42, name: 'Thüringen', countryId: 2 },
 
     // Austria Bundesländer
     { id: 43, name: 'Wien', countryId: 3 },
     { id: 44, name: 'Tirol', countryId: 3 },
     { id: 45, name: 'Steiermark', countryId: 3 },
-    // ... add all 9 Bundesländer
+    { id: 46, name: 'Vorarlberg', countryId: 3 },
+    { id: 47, name: 'Salzburg', countryId: 3 },
+    { id: 48, name: 'Oberösterreich', countryId: 3 },
+    { id: 49, name: 'Niederösterreich', countryId: 3 },
+    { id: 50, name: 'Kärnten', countryId: 3 },
+    { id: 51, name: 'Burgenland', countryId: 3 }
+
 ];
 
 export const parties: Party[] = [
-    // Swiss Parties
-    { id: 1, name: 'Schweizerische Volkspartei', abbreviation: 'SVP', color: '#008000', countryId: 1 },
-    { id: 2, name: 'Sozialdemokratische Partei der Schweiz', abbreviation: 'SP', color: '#D52B1E', countryId: 1 },
-    { id: 3, name: 'FDP.Die Liberalen', abbreviation: 'FDP', color: '#005F9E', countryId: 1 },
-    { id: 4, name: 'Die Mitte', abbreviation: 'Mitte', color: '#FF7F00', countryId: 1 },
-    { id: 5, name: 'GRÜNE Schweiz', abbreviation: 'GPS', color: '#64A12D', countryId: 1 },
-    { id: 6, name: 'Grünliberale Partei', abbreviation: 'GLP', color: '#BBE440', countryId: 1 },
-    { id: 7, name: 'Evangelische Volkspartei der Schweiz', abbreviation: 'EVP', color: '#FFD700', countryId: 1 },
-    { id: 8, name: 'Eidgenössisch-Demokratische Union', abbreviation: 'EDU', color: '#800080', countryId: 1 },
-    { id: 9, name: 'Alternative Liste', abbreviation: 'AL', color: '#FF00FF', countryId: 1 },
-    // ... add other Swiss parties
+// Swiss Parties
+    { id: 1, name: 'Schweizerische Volkspartei', abbreviation: 'SVP', color: '#008000', countryId: 1, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 2, name: 'Sozialdemokratische Partei der Schweiz', abbreviation: 'SP', color: '#D52B1E', countryId: 1, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 3, name: 'FDP.Die Liberalen', abbreviation: 'FDP', color: '#005F9E', countryId: 1, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 4, name: 'Die Mitte', abbreviation: 'Mitte', color: '#FF7F00', countryId: 1, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 5, name: 'GRÜNE Schweiz', abbreviation: 'GPS', color: '#64A12D', countryId: 1, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 6, name: 'Grünliberale Partei', abbreviation: 'GLP', color: '#BBE440', countryId: 1, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 7, name: 'Evangelische Volkspartei der Schweiz', abbreviation: 'EVP', color: '#FFD700', countryId: 1, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 8, name: 'Eidgenössisch-Demokratische Union', abbreviation: 'EDU', color: '#800080', countryId: 1, group: 'Kleine Rechtsparteien', groupAbbr: 'TBR' },
+    { id: 9, name: 'Alternative Liste', abbreviation: 'AL', color: '#FF00FF', countryId: 1, group: 'Kleine Linksparteien', groupAbbr: 'LGP' },
+    { id: 10, name: 'Partei der Arbeit', abbreviation: 'PdA', color: '#FF0000', countryId: 1, group: 'Kleine Linksparteien', groupAbbr: 'LGP' },
+    { id: 11, name: 'Solidarität', abbreviation: 'Sol.', color: '#FF4500', countryId: 1, group: 'Kleine Linksparteien', groupAbbr: 'LGP' },
+    { id: 12, name: 'Lega dei Ticinesi', abbreviation: 'Lega', color: '#0000FF', countryId: 1, group: 'Kleine Rechtsparteien', groupAbbr: 'TBR' },
+    { id: 13, name: 'Mouvement Citoyens Genevois', abbreviation: 'MCG', color: '#FFD700', countryId: 1, group: 'Kleine Rechtsparteien', groupAbbr: 'TBR' },
+    { id: 14, name: 'Christlichdemokratische Volkspartei', abbreviation: 'CVP', color: '#FFA500', countryId: 1, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 15, name: 'Bürgerlich-Demokratische Partei', abbreviation: 'BDP', color: '#CCCC00', countryId: 1, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 16, name: 'Freisinnig-Demokratische Partei', abbreviation: 'LPS', color: '#000080', countryId: 1, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 17, name: 'Frauen für den Frieden', abbreviation: 'FGA', color: '#008B8B', countryId: 1, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 18, name: 'Progressive Organisationen', abbreviation: 'POCH', color: '#696969', countryId: 1, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 19, name: 'Parteilos', abbreviation: 'PL', color: '#eaeaea', countryId: 1, group: 'Parteilos', groupAbbr: 'PL' },
 
-    // German Parties
-    { id: 10, name: 'Christlich Demokratische Union Deutschlands', abbreviation: 'CDU', color: '#000000', countryId: 2 },
-    { id: 11, name: 'Christlich-Soziale Union in Bayern', abbreviation: 'CSU', color: '#008AC5', countryId: 2 },
-    { id: 12, name: 'Sozialdemokratische Partei Deutschlands', abbreviation: 'SPD', color: '#E3000F', countryId: 2 },
-    { id: 13, name: 'Freie Demokratische Partei', abbreviation: 'FDP', color: '#FFED00', countryId: 2 },
-    { id: 14, name: 'Bündnis 90/Die Grünen', abbreviation: 'Grüne', color: '#64A12D', countryId: 2 },
-    { id: 15, name: 'Die Linke', abbreviation: 'Linke', color: '#BE3075', countryId: 2 },
-    { id: 16, name: 'Alternative für Deutschland', abbreviation: 'AfD', color: '#009EE0', countryId: 2 },
-    // ... add other German parties as needed
+// German Parties
+    { id: 20, name: 'Christlich Demokratische Union Deutschlands', abbreviation: 'CDU', color: '#000000', countryId: 2, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 21, name: 'Christlich-Soziale Union in Bayern', abbreviation: 'CSU', color: '#008AC5', countryId: 2, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 22, name: 'Sozialdemokratische Partei Deutschlands', abbreviation: 'SPD', color: '#E3000F', countryId: 2, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 23, name: 'Freie Demokratische Partei', abbreviation: 'FDP', color: '#FFED00', countryId: 2, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 24, name: 'Bündnis 90/Die Grünen', abbreviation: 'Grüne', color: '#64A12D', countryId: 2, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 25, name: 'Die Linke', abbreviation: 'Linke', color: '#BE3075', countryId: 2, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 26, name: 'Alternative für Deutschland', abbreviation: 'AfD', color: '#009EE0', countryId: 2, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 27, name: 'Piratenpartei Deutschland', abbreviation: 'Piraten', color: '#FF8800', countryId: 2, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 28, name: 'Partei Mensch Umwelt Tierschutz', abbreviation: 'Tierschutzpartei', color: '#008000', countryId: 2, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 29, name: 'Ökologisch-Demokratische Partei', abbreviation: 'ÖDP', color: '#FF6600', countryId: 2, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 30, name: 'Die Partei', abbreviation: 'Die PARTEI', color: '#DD0000', countryId: 2, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
 
-    // Austrian Parties
-    { id: 20, name: 'Österreichische Volkspartei', abbreviation: 'ÖVP', color: '#63C3D0', countryId: 3 },
-    { id: 21, name: 'Sozialdemokratische Partei Österreichs', abbreviation: 'SPÖ', color: '#E2231A', countryId: 3 },
-    { id: 22, name: 'Freiheitliche Partei Österreichs', abbreviation: 'FPÖ', color: '#0E428E', countryId: 3 },
-    { id: 23, name: 'Die Grünen – Die Grüne Alternative', abbreviation: 'Grüne', color: '#6BA539', countryId: 3 },
-    { id: 24, name: 'NEOS – Das Neue Österreich und Liberales Forum', abbreviation: 'NEOS', color: '#E91C80', countryId: 3 },
+// Austrian Parties
+    { id: 31, name: 'Österreichische Volkspartei', abbreviation: 'ÖVP', color: '#63C3D0', countryId: 3, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 32, name: 'Sozialdemokratische Partei Österreichs', abbreviation: 'SPÖ', color: '#E2231A', countryId: 3, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 33, name: 'Freiheitliche Partei Österreichs', abbreviation: 'FPÖ', color: '#0E428E', countryId: 3, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+    { id: 34, name: 'Die Grünen – Die Grüne Alternative', abbreviation: 'Grüne', color: '#6BA539', countryId: 3, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 35, name: 'NEOS – Das Neue Österreich und Liberales Forum', abbreviation: 'NEOS', color: '#E91C80', countryId: 3, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 36, name: 'Kommunistische Partei Österreichs', abbreviation: 'KPÖ', color: '#BE4B48', countryId: 3, group: 'Linke und grüne Parteien', groupAbbr: 'LGP' },
+    { id: 37, name: 'Liste Pilz', abbreviation: 'JETZT', color: '#0072B8', countryId: 3, group: 'Kleine Mitteparteien und andere', groupAbbr: 'KMUA' },
+    { id: 38, name: 'Bündnis Zukunft Österreich', abbreviation: 'BZÖ', color: '#FF9900', countryId: 3, group: 'Traditionelle bürgerliche und rechte Parteien', groupAbbr: 'TBR' },
+
     // ... add other Austrian parties as needed
 ];
 
 export const seatDistributions: SeatDistribution[] = [
     // Seat distributions for regions
-    // For Zurich, Switzerland
-    { id: 1, regionId: 1, partyId: 1, seats: 50 }, // SVP
-    { id: 2, regionId: 1, partyId: 2, seats: 40 }, // SP
-    { id: 3, regionId: 1, partyId: 3, seats: 30 }, // FDP
-    { id: 4, regionId: 1, partyId: 4, seats: 20 }, // Mitte
-    { id: 5, regionId: 1, partyId: 5, seats: 15 }, // GPS
-    { id: 6, regionId: 1, partyId: 6, seats: 10 }, // GLP
-    { id: 7, regionId: 1, partyId: 7, seats: 5 },  // EVP
-    { id: 8, regionId: 1, partyId: 8, seats: 2 },  // EDU
-    { id: 9, regionId: 1, partyId: 9, seats: 1 },  // AL
+
+    // Zürich, CH
+    { id: 1, regionId: 1, partyId: 1, seats: 46 }, // SVP
+    { id: 2, regionId: 1, partyId: 2, seats: 36 }, // SP
+    { id: 3, regionId: 1, partyId: 3, seats: 29 }, // FDP
+    { id: 4, regionId: 1, partyId: 4, seats: 11 }, // Mitte
+    { id: 5, regionId: 1, partyId: 5, seats: 19 }, // GPS
+    { id: 6, regionId: 1, partyId: 6, seats: 24 }, // GLP
+    { id: 7, regionId: 1, partyId: 7, seats: 7 },  // EVP
+    { id: 8, regionId: 1, partyId: 8, seats: 3 },  // EDU
+    { id: 9, regionId: 1, partyId: 9, seats: 5 },  // AL
+
+    // Zug, CH
+    { id: 1, regionId: 2, partyId: 1, seats: 18 }, // SVP
+    { id: 2, regionId: 2, partyId: 2, seats: 8 }, // SP
+    { id: 3, regionId: 2, partyId: 3, seats: 18 }, // FDP
+    { id: 4, regionId: 2, partyId: 4, seats: 19 }, // Mitte
+    { id: 5, regionId: 2, partyId: 5, seats: 11 }, // GPS
+    { id: 6, regionId: 2, partyId: 6, seats: 6 }, // GLP
+    { id: 7, regionId: 2, partyId: 19, seats: 20 }, //PL
+
+    // Aargau, CH
+    { id: 1, regionId: 3, partyId: 1, seats: 48 }, // SVP
+    { id: 2, regionId: 3, partyId: 2, seats: 23 }, // SP
+    { id: 3, regionId: 3, partyId: 3, seats: 22 }, // FDP
+    { id: 4, regionId: 3, partyId: 4, seats: 18 }, // Mitte
+    { id: 5, regionId: 3, partyId: 5, seats: 10 }, // GPS
+    { id: 6, regionId: 3, partyId: 6, seats: 11 }, // GLP
+    { id: 7, regionId: 3, partyId: 7, seats: 5 },  // EVP
+    { id: 8, regionId: 3, partyId: 8, seats: 3 },  // EDU
+
+    // AR, CH
+    { id: 1, regionId: 4, partyId: 1, seats: 7 }, // SVP
+    { id: 2, regionId: 4, partyId: 2, seats: 9 }, // SP
+    { id: 3, regionId: 4, partyId: 3, seats: 22 }, // FDP
+    { id: 4, regionId: 4, partyId: 4, seats: 3 }, // Mitte
+    { id: 5, regionId: 4, partyId: 5, seats: 0 }, // GPS
+    { id: 6, regionId: 4, partyId: 6, seats: 2 }, // GLP
+    { id: 7, regionId: 4, partyId: 7, seats: 2 },  // EVP
+    { id: 8, regionId: 4, partyId: 7, seats: 2 },  // EVP
 
     // For Bavaria, Germany
     { id: 100, regionId: 27, partyId: 11, seats: 85 }, // CSU
